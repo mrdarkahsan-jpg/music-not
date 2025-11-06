1552892c997af510a61c9af4fa6a20c772e6a234
mrdarkahsan-jpg <mrdarkahsan@gmail.com>
2025-11-06 15:03:13 +0600

    Update config.js

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ class EnterpriseConfigurationManager {
              * Configure your Lavalink server for audio processing
              */
             lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "87.106.208.203", 
-                port: EnvironmentVariableProcessor.LAVALINK_PORT || 14515,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "susara", 
+                host: EnvironmentVariableProcessor.LAVALINK_HOST || "192.99.10.133", 
+                port: EnvironmentVariableProcessor.LAVALINK_PORT || 5140,       
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "friston", 
                 secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
             },
             
@@ -101,5 +101,6 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
